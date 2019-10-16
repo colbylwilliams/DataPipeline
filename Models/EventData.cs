521589093b6535a6f69655ec618935a1704c0a45
Colby Williams <ColbyLWilliams@gmail.com>
2019-10-16 15:41:40 -0400

    lowercase property names in DB

--- Models/EventData.cs
+++ Models/EventData.cs
@@ -1,11 +1,17 @@
 using Microsoft.Azure.Documents;
+using Newtonsoft.Json;
 
 namespace DataPipeline
 {
     public class EventData : Document
     {
+        [JsonProperty("title")]
         public string Title { get; set; }
+
+        [JsonProperty("description")]
         public string Description { get; set; }
+
+        [JsonProperty("location")]
         public string Location { get; set; }
     }
 }
